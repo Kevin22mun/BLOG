37f32c33e772beb3f23bbda1d2cb1fc4ef246ec0
kevin <kevin.munoz@ema.org.mx>
2022-06-23 10:48:07 -0500

    Se agrego la url de BLOG

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,11 +18,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
+
 #from apps.blog import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/',include ('blog.urls',namespace = "blog")),
     path('api-auth/', include('rest_framework.urls')),
 ]
 
